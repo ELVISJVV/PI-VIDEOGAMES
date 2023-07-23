c9bfb8c079ed49982b43884563244ebd91746cf1
ELVISJVV <elvisvasquez257@gmail.com>
2023-07-23 17:30:48 -0600

    models created

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,47 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    
+    id:{
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  
+    release_date:{
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    platforms:{
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    image:{
+      type: DataTypes.STRING,
+      allowNull: false
+    
+    },
+    rating:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    genres:{
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+
+  }, { timestamps: false });
 };
