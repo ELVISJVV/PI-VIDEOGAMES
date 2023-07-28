3a367f7893703677314f3d02d90a133bbb217c17
ELVISJVV <elvisvasquez257@gmail.com>
2023-07-27 23:16:53 -0600

    optimizing code get games

--- api/src/controllers/VideoGames/getAllGames.js
+++ api/src/controllers/VideoGames/getAllGames.js
@@ -5,28 +5,65 @@ const { cleanArray, cleanArrayDatabase } = require('./utils.js');
 const { Videogame,Genre } = require('../../db.js');
 
 
+// unoptimized code
+// const getVideogames = async () => {
+//     let games = [];
+//     let i = 1;
+//     const databaseVideogames = await Videogame.findAll({
+//         include: Genre,
+//     });
+
+//     const databaseVideogamesClean = cleanArrayDatabase(databaseVideogames);
+
+//     while (i <= 5) {
+//         const apiVideogames = (await axios.get(`${BASE_URL}?key=${API_KEY}&page=${i}`)).data.results;
+//         games = [...games, ...apiVideogames]
+//         i++
+//     }
+
+
+
+//     const videogames = cleanArray(games);
+
+//     return [...databaseVideogamesClean, ...videogames]
+
+// }
+
+
+// optimized code
+
 const getVideogames = async () => {
+    const totalPages = 5; // Total number of API pages
+
     let games = [];
-    let i = 1;
+
+    // Using Promise.all() to make concurrent API calls
+    const apiPromises = [];
+    for (let i = 1; i <= totalPages; i++) {
+        apiPromises.push(axios.get(`${BASE_URL}?key=${API_KEY}&page=${i}`));
+    }
+
+    try {
+        const apiResponses = await Promise.all(apiPromises);
+        apiResponses.forEach((response) => {
+            games = [...games, ...response.data.results];
+        });
+    } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+    }
+
+    // Get the games from the database
     const databaseVideogames = await Videogame.findAll({
         include: Genre,
     });
 
     const databaseVideogamesClean = cleanArrayDatabase(databaseVideogames);
-  
-    while (i <= 20) {
-        const apiVideogames = (await axios.get(`${BASE_URL}?key=${API_KEY}&page=${i}`)).data.results;
-        games = [...games, ...apiVideogames]
-        i++
-    }
-
-
 
     const videogames = cleanArray(games);
 
-    return [...databaseVideogamesClean, ...videogames]
-
+    return [...databaseVideogamesClean, ...videogames];
 }
+
 
 
 module.exports = {
--- api/src/controllers/VideoGames/getGameById.js
+++ api/src/controllers/VideoGames/getGameById.js
@@ -2,7 +2,9 @@ const {API_KEY, BASE_URL} = process.env;
 const axios = require('axios');
 
 
-const getVideogames = async () => {
+const getVideogames = async (id) => {
+
+
     const databaseVideogames = await Videogame.findAll();
 
     const apiVideogames = (await axios.get(`${BASE_URL}?key=${API_KEY}`)).results;
