4ebe67c8a001d41952e7f660b06fe971ed06af18
ELVISJVV <elvisvasquez257@gmail.com>
2023-08-01 02:01:28 -0600

    new actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -3,7 +3,12 @@ import {
     GET_VIDEOGAME_BY_ID,
     GET_GENRES,
     GET_VIDEOGAMES_BY_NAME,
-    POST_VIDEOGAME
+    POST_VIDEOGAME,
+    FILTER_BY_GENRE,
+    FILTER_BY_ORIGIN,
+    SORT_BY_NAME,
+    SORT_BY_RATING,
+    RESET_FILTERS
 } from "./actionTypes";
 import axios from "axios";
 
@@ -51,61 +56,96 @@ export const getVideogameById = (id) => {
 }
 
 export const getGenres = () => {
-    
-        return async (dispatch) => {
-            try {
-                const response = await axios.get('http://localhost:3001/genres');
-                const genres = response.data;
-    
-                dispatch({
-                    type: GET_GENRES,
-                    payload: genres
-                })
-    
-            } catch (error) {
-                console.error('Error al obtener los generos:', error);
-            }
-    
+
+    return async (dispatch) => {
+        try {
+            const response = await axios.get('http://localhost:3001/genres');
+            const genres = response.data;
+
+            dispatch({
+                type: GET_GENRES,
+                payload: genres
+            })
+
+        } catch (error) {
+            console.error('Error al obtener los generos:', error);
         }
-    
+
+    }
+
 }
 
 export const getVideogamesByName = (name) => {
-    
-        return async (dispatch) => {
-            try {
-                const response = await axios.get(`http://localhost:3001/videogames?name=${name}`);
-                const videogames = response.data;
-    
-                dispatch({
-                    type: GET_VIDEOGAMES_BY_NAME,
-                    payload: videogames
-                })
-    
-            } catch (error) {
-                console.error('Error al obtener los videojuegos:', error);
-            }
-    
+
+    return async (dispatch) => {
+        try {
+            const response = await axios.get(`http://localhost:3001/videogames?name=${name}`);
+            const videogames = response.data;
+
+            dispatch({
+                type: GET_VIDEOGAMES_BY_NAME,
+                payload: videogames
+            })
+
+        } catch (error) {
+            console.error('Error al obtener los videojuegos:', error);
         }
-    
+
+    }
+
 }
 
 export const postVideogame = (videogame) => {
-        
-            return async (dispatch) => {
-                try {
-                    const response = await axios.post('http://localhost:3001/videogames', videogame);
-                    
 
-                    dispatch({
-                        type: POST_VIDEOGAME,
-                        payload: response.data
-                    })
+    return async (dispatch) => {
+        try {
+            const response = await axios.post('http://localhost:3001/videogames', videogame);
 
-                } catch (error) {
-                    console.error('Error al crear el videojuego:', error);
-                    
-                }
-        
-            }
+
+            dispatch({
+                type: POST_VIDEOGAME,
+                payload: response.data
+            })
+
+        } catch (error) {
+            console.error('Error al crear el videojuego:', error);
+
+        }
+
+    }
+}
+
+export const filterByGenre = (genre) => {
+    return {
+        type: FILTER_BY_GENRE,
+        payload: genre
+    }
+}
+
+export const filterByOrigin = (origin) => {
+    return {
+        type: FILTER_BY_ORIGIN,
+        payload: origin
+    }
+}
+
+export const sortByName = (order) => {
+    return {
+        type: SORT_BY_NAME,
+        payload: order
+    }
+}
+
+export const sortByRating = (order) => {
+    return {
+        type: SORT_BY_RATING,
+        payload: order
+    }
+}
+
+
+export const resetFilters = () => {
+    return {
+        type: RESET_FILTERS
+    }
 }
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -3,50 +3,119 @@ import {
     GET_VIDEOGAME_BY_ID,
     GET_GENRES,
     GET_VIDEOGAMES_BY_NAME,
-    POST_VIDEOGAME
+    POST_VIDEOGAME,
+    FILTER_BY_GENRE,
+    FILTER_BY_ORIGIN,
+    SORT_BY_NAME,
+    SORT_BY_RATING,
+    RESET_FILTERS
 } from "./actionTypes";
 
 const initialState = {
     videogames: [],
+    allVideogames: [],
     videogameDetail: {},
     genres: [],
     videogamesByName: [],
-    videogameCreated: {}
-
+    videogameCreated: {},
+    filteredByGenre: [],
+    filteredByOrigin: [],
+    sortedByName: [],
+    sortedByRating: []
 };
 
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_VIDEOGAMES:
-        return{
-            ...state,
-            videogames: action.payload
-        }
-        
+            return {
+                ...state,
+                allVideogames: action.payload,
+                videogames: action.payload
+            }
+
         case GET_VIDEOGAME_BY_ID:
-        return{
-            ...state,
-            videogameDetail: action.payload
-        }
+            return {
+                ...state,
+                videogameDetail: action.payload
+            }
 
         case GET_GENRES:
-        return{
-            ...state,
-            genres: action.payload
-        }
+            return {
+                ...state,
+                genres: action.payload
+            }
 
         case GET_VIDEOGAMES_BY_NAME:
-        return{
-            ...state,
-            videogames: action.payload
-        }
+            return {
+                ...state,
+                videogames: action.payload,
+                videogamesByName: action.payload
+            }
 
         case POST_VIDEOGAME:
-        return{
-            ...state,
-            videogameCreated: action.payload
-        }
+            return {
+                ...state,
+                videogameCreated: action.payload
+            }
+
+        case FILTER_BY_GENRE:
+            return {
+                ...state,
+                videogames: action.payload,
+                filteredByGenre: action.payload
+            }
+
+        case FILTER_BY_ORIGIN:
+            return {
+                ...state,
+                videogames: action.payload,
+                filteredByOrigin: action.payload
+            }
+
+        case SORT_BY_NAME:
+
+            let sortedByName = [];
+            if (action.payload === 'ascendingName') {
+                sortedByName = [...state.videogames].sort((a, b) => a.name.localeCompare(b.name));
+            }
+
+            if (action.payload === 'descendingName') {
+                sortedByName = [...state.videogames].sort((a, b) => b.name.localeCompare(a.name));
+            }
+
+            return {
+                ...state,
+                videogames: sortedByName,
+                sortedByName: sortedByName
+            };
+
+        case SORT_BY_RATING:
+            let sortedByRating = [];
+            if (action.payload === 'ascendingRating') {
+                sortedByRating = [...state.videogames].sort((a, b) => a.rating - b.rating);
+            }
+
+            if (action.payload === 'descendingRating') {
+                sortedByRating = [...state.videogames].sort((a, b) => b.rating - a.rating);
+            }
+
+            return {
+                ...state,
+                videogames: sortedByRating,
+                sortedByRating: sortedByRating
+            };
+
+        case RESET_FILTERS:
+            return {
+                ...state,
+                videogames: state.allVideogames,
+                filteredByGenre: [],
+                filteredByOrigin: [],
+                sortedByName: [],
+                sortedByRating: []
+            }
+
 
         default:
             return state;
